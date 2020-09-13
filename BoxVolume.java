7693cd6c726377c846d94fce2b47e4276466471e
theserdzo <theserdzo@gmail.com>
2020-09-13 23:28:41 +0200

    Box and Date Updated

--- BoxVolume.java
+++ BoxVolume.java
@@ -1,6 +1,29 @@
-class BoxVolume { 
-    public static void main(String[] args) 
-    { 
-        System.out.println("BoxVolume"); 
-    } 
-} 
+import java.util.Scanner;
+/*
+Hint: The java.util.Scanner class offers a method for capturing a string from the console.
+String str = new Scanner(System.in).nextInt();
+*/
+public class BoxVolume
+{
+ public static void main(String[] args)
+ {
+   int width, length, height;
+   int volume;
+   Scanner scan = new Scanner(System.in);
+   
+   System.out.println("Please enter the width of your box: ");
+   width = scan.nextInt();
+   
+   System.out.println("Please enter the height of your box: ");
+   height = scan.nextInt();
+   
+   System.out.println("Please enter the length of your box: ");
+   length = scan.nextInt();
+   
+   volume = width * length * height;
+      
+   System.out.println("Box Dimensions: " );
+   System.out.println("Height: " + height + " Width " + width + " Length " + length);
+   System.out.println("Box Volume: " + volume);   
+ } 
+}
