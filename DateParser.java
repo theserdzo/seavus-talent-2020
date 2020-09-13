f7dae64ed8890af2225eea866e438296fb01fcf2
theserdzo <theserdzo@gmail.com>
2020-09-13 20:59:01 +0200

    DateParser

--- DateParser.java
+++ DateParser.java
@@ -1,6 +1,15 @@
-class DateParser { 
-    public static void main(String[] args) 
-    { 
-        System.out.println("DateParser"); 
-    } 
-} 
+import java.util.Scanner;
+
+public class DateParser
+{
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a date in dd/mm/yyyy format: ");
+        
+        String str = new Scanner(System.in).nextLine();
+        for (String dates: str.split("/")) {
+            System.out.println("\n"+dates);
+        }
+    }
+}
