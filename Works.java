ae1ed0c0b488baa2ec7187b0a2a26b0379b63bac
theserdzo <theserdzo@gmail.com>
2020-09-14 02:03:19 +0200

    Java14Env

--- Works.java
+++ Works.java
@@ -1,9 +1,5 @@
 public class Works {
-/**
-* The HelloWorldApp class implements an application
-* that simply prints "Hello Java Developers!" message to standard output. *
-* @param args
-*/
+
       public static void main(String[] args) {
 System.out.println("Hello Java Developers!"); }
 }
